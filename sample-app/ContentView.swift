ab0fb90430cac221e29abc4e27014b5743ca0bda
ravuga <gauravkumarak@gmail.com>
2021-01-17 00:16:34 +0530

    First Sample Complete

---
+++ sample-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  sample-app
+//
+//  Created by Gaurav Kumar on 16/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ sample-app/sample_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  sample_appApp.swift
+//  sample-app
+//
+//  Created by Gaurav Kumar on 16/01/21.
+//
+
+import SwiftUI
+
+@main
+struct sample_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
